d517d4bfbf200d204a40c438ef867b020b47900c
anushree <anushreecd2000@gmail.com>
2023-08-17 18:25:46 +0530

    second project

---
+++ components/nav.js
@@ -0,0 +1,11 @@
+const nav = () =>{
+    return`<div id="nav">
+    <a href="/index.html">home</a>
+    <a href="/pages/product.html">Product</a>
+    <a href="/pages/cart.html">Cart</a>
+    <a href="/pages/signin.html">Sign In</a>
+    <a href="/pages/signup.html">Sign Up</a>
+    </div>`
+}
+
+export default nav
---
+++ javascript/index.js
@@ -0,0 +1,4 @@
+import nav from '../components/nav.js'
+
+
+document.getElementById("navbar").innerHTML=nav()
