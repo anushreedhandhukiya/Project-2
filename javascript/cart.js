9270a92c1d5c92461f3ddd99e625091d1f0955be
anushree <anushreecd2000@gmail.com>
2023-08-23 23:03:21 +0530

    cart page

--- javascript/cart.js
+++ javascript/cart.js
@@ -2,65 +2,89 @@ import nav from "../components/nav.js";
 
 document.getElementById("navbar").innerHTML = nav()
 
-const minusButton = document.getElementById("minus");
-const plusButton = document.getElementById("plus");
-const quantityDisplay = document.getElementById("quantity");
-const totalPriceDisplay = document.getElementById("totalPrice");
-let price = 10;
-let quantity = 1;
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let totalprice = 0;
+cart.map((item) => {
+  totalprice += item.price * item.qty * 10;
+});
 
-const display = (data) => {
-    data.map((item) => {
-        let image = document.createElement("img")
-        image.src = item.image
-        let title = document.createElement("h3")
-        title.innerHTML = item.title
-        let category = document.createElement("p")
-        category.innerHTML = item.category
-        let rating = document.createElement("h5")
-        let minus = document.createElement("h2")
-        minus.innerHTML = "-"
-        minusButton.addEventListener("click", () => {
-            if (quantity > 1) {
-                quantity--;
-                quantityDisplay.textContent = quantity;
-                updateTotalPrice();
-            }
-        });
-        let quantity = document.createElement("h2")
-        quantity.innerHTML = item.quantity
-        let plus = document.createElement("h2")
-        plus.innerHTML = "+"
-        plusButton.addEventListener("click", () => {
-            quantity++;
-            quantityDisplay.textContent = quantity;
-            updateTotalPrice();
-        });
-        let Price = document.createElement("h3")
-        Price.innerHTML= item.Price
-        function updateTotalPrice() {
-            const total = price * quantity;
-            totalPriceDisplay.textContent = `$${total}`;
-        }
-        let btn = document.createElement("button")
-        btn.innerHTML = "Buy Now"
-        let btn1 = document.createElement("button")
-        btn1.innerHTML = "delete"
-        btn1.addEventListener("click", () => {
-            fetch(`http://localhost:3000/cart/${item.id}`, {
-                method: "DELETE",
-            })
-        })
-        let div = document.createElement("div")
-        div.append(image, title, category, rating, minus, quantity, plus,Price, btn,btn1)
-        document.getElementById("product-box").append(div)
-    })
-}
-const get = () => {
-    fetch("http://localhost:3000/cart")
-        .then((response) => response.json())
-        .then((response) => {
-            display(response)
-        })
-}
-get()
+
+let cart1 = (cart) => {
+
+  cart.map((item, index) => {
+
+    let mainbox = document.createElement("div");
+
+    let imgdiv = document.createElement("div");
+    let image = document.createElement("img");
+    image.src = item.image;
+
+    let texts = document.createElement("div");
+
+    let title = document.createElement("h3");
+    title.innerHTML = item.title;
+
+    let minse = document.createElement("button");
+    minse.innerHTML = "-";
+
+    let price1 = document.createElement("p");
+    price1.innerHTML = `${item.price * 10}$`;
+
+    let plusminse = document.createElement("div");
+
+    let btns = document.createElement("button");
+    btns.innerHTML = item.qty;
+
+    let plus = document.createElement("button");
+    plus.innerHTML = "+";
+
+    imgdiv.append(image);
+    texts.append(title, price1);
+    plusminse.append(minse, btns, plus);
+    mainbox.append(imgdiv, texts, plusminse);
+    document.getElementById("cartpage").append(mainbox);
+
+    price1.setAttribute("class", "price1");
+    mainbox.setAttribute("class", "mainbox");
+    imgdiv.setAttribute("class", "imgdiv");
+    texts.setAttribute("class", "texts");
+    plusminse.setAttribute("class", "plusminse");
+
+    minse.addEventListener("click", () => {
+      console.log(item);
+      let qty = cart[index].qty;
+      if (qty == 1) {
+        cart.splice(index, 1);
+        localStorage.setItem("cart", JSON.stringify(cart));
+
+        console.log(cart);
+        window.location.reload();
+      } else {
+        cart[index].qty -= 1;
+        localStorage.setItem("cart", JSON.stringify(cart));
+        window.location.reload();
+      }
+    });
+    plus.addEventListener("click", () => {
+      console.log("test");
+      cart[index].qty += 1;
+      localStorage.setItem("cart", JSON.stringify(cart));
+      window.location.reload();
+    });
+  });
+};
+cart1(cart);
+
+let cart2 = () => {
+  let mainbox2 = document.createElement("div");
+
+  let lable = document.createElement("label");
+  lable.innerHTML = " PRODUCT TOTAL PRICE :";
+
+  document.getElementById("other").append(mainbox2);
+
+  document.getElementById("price").append(lable, totalprice);
+
+  mainbox2.setAttribute("class", "mainbox2");
+};
+cart2();
--- javascript/product.js
+++ javascript/product.js
@@ -1,152 +1,113 @@
 import nav from "../components/nav.js";
 document.getElementById("navbar").innerHTML=nav()
 
-const display=(data)=>{
-    data.map((item)=>{
-        let image = document.createElement("img")
-        image.src = item.image
-        let title = document.createElement("h3")
-        title.innerHTML = item.title
-        let price = document.createElement("h4")
-        price.innerHTML = item.price
-        let category = document.createElement("p")
-        category.innerHTML = item.category
-        let rating = document.createElement("h5")
 
-        if(item.rating.rate > 4){
-            rating.innerHTML = "*****"
-            rating.style.color = "green"
+const display = (data) => {
+    const box2 = document.getElementById("box2");
+    box2.innerHTML = ""; // Clear the existing content
+  
+    data.forEach((item) => {
+      const div = document.createElement("div");
+      div.classList.add("product-box");
+  
+      const image = document.createElement("img");
+      image.src = item.image;
+  
+      const title = document.createElement("h3");
+      title.textContent = item.title;
+  
+      const price = document.createElement("h3");
+      price.textContent = `${item.price}$`;
+  
+      const category = document.createElement("h3");
+      category.textContent = item.category;
+  
+      const rating = document.createElement("h4");
+      rating.textContent = "* ".repeat(item.rating.rate);
+  
+      if(item.rating.rate>4)
+          {
+              rating.innerHTML="* * * * *"
+              rating.style.color="green"
+          }
+          else if(item.rating.rate<=4 && item.rating.rate>=3)
+          {
+              rating.innerHTML="* * * *"
+              rating.style.color="chocolate"   
+          }
+          else{
+              rating.innerHTML="* *"
+              rating.style.color="red" 
+          }
+      const btn1 = document.createElement("button");
+      btn1.textContent = "Add to Cart";
+      btn1.classList.add("btn1");
+      div.append(image, title, price, category, rating, btn1);
+      box2.appendChild(div);
+
+      btn1.addEventListener("click", () => {
+        let cart = JSON.parse(localStorage.getItem("cart")) || [];
+        let exists = false;
+  
+        cart.forEach((cartItem, index) => {
+          if (cartItem.id === item.id) {
+            cart[index].qty += 1;
+            exists = true;
+          }
+        });
+  
+        if (!exists) {
+          cart.push({ ...item, qty: 1 });
+          alert("Product was added Successfully");
         }
-        else if(item.rating.rate <= 4 && item.rating.rate >= 3){
-            rating.innerHTML = "****"
-            rating.style.color = "darkorange"
-        }
-        else{
-            rating.innerHTML = "**"
-            rating.style.color = "red"
-        }
-        let btn = document.createElement("button")
-        btn.innerHTML="Buy Now"
-        let btn2 = document.createElement("button")
-        btn2.innerHTML="Add to Cart"
-        btn2.addEventListener("click",()=>{
-            console.log(item.id);
-            let loggedIn = localStorage.getItem("loggedIn")
-            if(loggedIn){
-                fetch(`http://localhost:3000/cart?id=${item.id}`)
-                .then((response)=>response.json())
-                .then((data)=>{
-                    if(data.length > 0){
-                        alert("Product was added Successfully")
-                        data[0].qty=data[0].qlt+1
-                        console.log(data[0].qty);
-                        fetch(`http://localhost:3000/cart`,{
-                            method:"PATCH",
-                            headers: { "content-type": "application/json" },
-                            body: JSON.stringify(...data)
-                        })
-                    }
-                })
-                fetch("http://localhost:3000/cart", {
-                            method: "POST",
-                            headers: { "content-type": "application/json" },
-                            body: JSON.stringify(item)
-                        })
-                        window.location.href="/pages/cart.html"
-            }
-            else{
-                window.location.href="./pages/signin.html"
-            }
-        })
-        let div = document.createElement("div")
-        div.append(image,title,price,category,rating,btn,btn2)
-        document.getElementById("box2").append(div)
-    })
-}
-const get= ()=>{
+  
+        localStorage.setItem("cart", JSON.stringify(cart));
+      });
+    });
+  };
+  
+  let products = [];
+  
+  document.getElementById("lth").addEventListener("click", () => {
+    products.sort((a, b) => a.price - b.price);
+    display(products);
+  });
+  
+  document.getElementById("htl").addEventListener("click", () => {
+    products.sort((a, b) => b.price - a.price);
+    display(products);
+  });
+  
+  document.getElementById("man").addEventListener("click", () => {
+    const temp = products.filter((val) => val.category === "men's clothing");
+    display(temp);
+  });
+  
+  document.getElementById("woman").addEventListener("click", () => {
+    const temp = products.filter((val) => val.category === "women's clothing");
+    display(temp);
+  });
+  
+  document.getElementById("ele").addEventListener("click", () => {
+    const temp = products.filter((val) => val.category === "electronics");
+    display(temp);
+  });
+  
+  fetch("http://localhost:3000/product")
+    .then((response) => response.json())
+    .then((data) => {
+      products = data;
+      display(data);
+    });
+  
+  // Function to fetch and display products initially
+  const get = () => {
     fetch("http://localhost:3000/product")
-    .then((response)=>response.json())
-    .then((response)=>{
-        display(response)
-    })
-}
-get()
-
-// let openshopping = document.querySelector(".shopping")
-// let closeshopping = document.querySelector(".closeshopping") 
-// let list = document.querySelector(".list")
-// let listCard = document.querySelector(".listCard")
-// let body = document.querySelector("body")
-// let total = document.querySelector(".total")
-// let qty =document.querySelector(".qty")
-
-// openshopping.addEventListener("click",()=>{
-//     body.classList.add("active")
-// })
-
-// closeshopping.addEventListener("click",()=>{
-//     body.classList.remove("active")
-// })
-// let products= []
-// let listcards = []
-
-// function initApp(){
-//     products.forEach((value,key)=>{
-//         let newDiv = document.createElement("div")
-//         newDiv.classList.add("item")
-//         newDiv.innerHTML = `
-//         <img src="image/${value.image}"/>
-//         <div class="title>${value.name}</div>
-//         <div class="price>${value.price.toLocaleString()}</div>
-//         <button onclick="addtocard(${key})">Add to Card</button>
-//         `
-//         list.appendChild(newDiv)
-//     })
-// }
-// initApp()
-
-// function addtocart(key){
-//     if(listcards[key] == null){
-//         listcards[key] = products[key];
-//         listcards[key].qty = 1;
-//     }
-//     reloadcard()
-// }
-// function reloadcard() {
-//     listcard.innerHTML = "";
-//     let count = 0
-//     let totalprice= 0
-//     listcards.forEach((value,key)=>{
-//         totalprice = totalprice + value.price
-//         count = count + value.qty
-
-//         if(value != null){
-//             let newDiv = document.createElement("li")
-//             newDiv.innerHTML = `
-//                 <div><img src="image/${value.image}"/></div>
-//                 <div>${value.name}</div>
-//                 <div>${value.price.toLocaleString()}</div>
-//                 <div>${value.qty}</div>
-//                 <div>
-//                     <button onclick="changeQuantity(${key}, ${value.qty - 1})">-</button>
-//                     <div class="count">${value.qty}</div>
-//                     <button onclick="changeQuantity(${key}, ${value.qty + 1})">+</button>
-//                 </div>
-//             `
-//             listcard.appendChild(newDiv)
-//         }
-//     })
-//     total.innerText = totalprice.toLocaleString();
-//     qty.innerText = count;
-// }
-// function changeQuantity(key, qty){
-//     if(qty == 0){
-//         delete listcards[key];
-//     }
-//     else{
-//         listcards[key].qty = qty
-//         listcards[key].price = qty * products[key].price
-//     }
-//     reloadcard()
-// }
-
+      .then((response) => response.json())
+      .then((data) => {
+        products = data;
+        display(data);
+      });
+  };
+  
+  get();
