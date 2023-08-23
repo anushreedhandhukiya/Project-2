ac7239a72584b6dfa65319e32fc01357176550c0
anushree <anushreecd2000@gmail.com>
2023-08-23 12:49:23 +0530

    add to card

--- javascript/cart.js
+++ javascript/cart.js
@@ -13,11 +13,30 @@ const display = (data) => {
         let category = document.createElement("p")
         category.innerHTML = item.category
         let rating = document.createElement("h5")
+        let minus = document.createElement("h2")
+        minus.innerHTML = "-"
+        let num = document.createElement("h2")
+        num.innerHTML = "01"
+        let plus = document.createElement("h2")
+        plus.innerHTML = "+"
         let btn = document.createElement("button")
         btn.innerHTML = "Buy Now"
         let div = document.createElement("div")
-        div.append(image, title, price, category, rating, btn)
+        div.append(image, title, price, category, rating,minus,num,plus, btn)
         document.getElementById("product-box").append(div)
+        let a=1
+        plus.addEventListener("click",()=>{
+            a++
+            a = (a < 10) ? "0" + a : a;
+            num.innerHTML = a
+        })
+        minus.addEventListener("click",()=>{
+            if(a > 1){
+                a--
+                a = (a < 10) ? "0" + a : a;
+            num.innerHTML = a
+            }
+        })
     })
 }
 const get= ()=>{
